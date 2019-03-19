74fe2352250438d69fb6b4c139ffaf1753ff21a2
Ken Kousen <ken.kousen@kousenit.com>
2019-03-19 11:21:41 -0400

    Add verification of injected mock

--- src/main/java/com/oreilly/NumberCollection.java
+++ src/main/java/com/oreilly/NumberCollection.java
@@ -11,7 +11,8 @@ public class NumberCollection {
 
     public int getTotal() {
         int total = 0;
-        for (int i = 0; i < numbers.size(); i++) {
+        int count = numbers.size();
+        for (int i = 0; i < count; i++) {
             total += numbers.get(i);
         }
         return total;
